a584435a815f3f035d5cb39029651d7b14d928e8
Bob Houghton <behoughton@yahoo.com>
2020-03-12 12:20:36 -0500

    testing ajax

--- src/app.py
+++ src/app.py
@@ -116,7 +116,7 @@ def background_process():
     #try:
         lang = request.args.get('proglang', 0, type=str)
 
-        result = str(lang)
+        result = lang
         #result= Sched_Event.find_by_series(lang1)
         #if lang == 'python':
         #    return jsonify(result="you are correct")
