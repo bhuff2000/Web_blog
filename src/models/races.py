83a206da341ed374b90b8825aa51087c72b504e9
Bob Houghton <behoughton@yahoo.com>
2020-03-12 09:23:34 -0500

    testing ajax

--- src/models/races.py
+++ src/models/races.py
@@ -101,6 +101,6 @@ class Sched_Event(object):
         races_list = []
         for race in races:
             races_list.append(race)
-        one_race = {races_list[0]['race_name']}
-        result = {"result":one_race}
+        one_race = {races_list[0]}
+        result = {"result":one_race['race_name']}
         return result
