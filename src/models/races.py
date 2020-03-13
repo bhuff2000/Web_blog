5ed728aa69592af392550f7be892839c026873da
Bob Houghton <behoughton@yahoo.com>
2020-03-13 09:28:05 -0500

    ajax texting

--- src/models/races.py
+++ src/models/races.py
@@ -111,4 +111,4 @@ class Sched_Event(object):
         one_race = Database.find_one(collection='races',
                                  query={'race_id': race_id})
 
-        return cls(**one_race)
+        return one_race
