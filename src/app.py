edbfaa1fd3a0d484a574d1107ebb844cc40be01f
Bob Houghton <behoughton@yahoo.com>
2020-03-15 11:09:18 -0500

    ajax texting

--- src/app.py
+++ src/app.py
@@ -165,8 +165,8 @@ def interactive():
     cursor = Sched_Event.find_by_series(series)
     ser_list1 = loads(dumps(cursor))
     #ser_list = loads(ser_list1)
-    ser_list3 = ser_list1[0]["race_name"]
-    return render_template('interactive.html', ser_list=ser_list1)
+    ser_list3 = ser_list1[15]["race_name"]
+    return render_template('interactive.html', ser_list=ser_list3)
 
 
 if __name__ == '__main__':
