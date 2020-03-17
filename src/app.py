f368f8c7edf81bef5b27e6e9d1a1562ef4c66986
Bob Houghton <behoughton@yahoo.com>
2020-03-16 22:00:25 -0500

    added entrants object with sportradar and load to mongo

--- src/app.py
+++ src/app.py
@@ -147,9 +147,9 @@ def ajax_get_races():
 def nascar_load_entrants():
     type1 = 'races'
     race_id = request.form.get['race_drop_down_re']
-    series = request.form.get['series_drop_down_se']
-    file = 'entry_list.json'
-    data = Utils.get_from_sportradar(series, type1, race_id, file)
+   # series = request.form.get['series_drop_down_se']
+   # file = 'entry_list.json'
+   # data = Utils.get_from_sportradar(series, type1, race_id, file)
     #entrant_list = Entrants.extract_sportradar_data(data)
     #load_list = []
     #ignore_list = []
@@ -164,7 +164,7 @@ def nascar_load_entrants():
      #       ignore_list.append(entrant)
 
     #races = Database.find(collection="entrants", query={"race_id": race_id})
-    text = data
+    text = race_id
     #return render_template('drivers_load.html', text=text, entrants=load_list, ignore_list=ignore_list)
     return render_template('drivers_load.html', text=data)
 
