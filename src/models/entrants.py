0cbec231e906aad701be6b0dc194b2a4fa8b9ccc
Bob Houghton <behoughton@yahoo.com>
2020-03-19 13:55:39 -0500

    took out jquery command on nascar admin

--- src/models/entrants.py
+++ src/models/entrants.py
@@ -58,7 +58,7 @@ class Entrants(object):
         #year = json_file['season']['year']
         for entrant in json_file2:
             car_num = entrant['car']['number']
-            if entrant['car']['crew_chief'] is not None:
+            if entrant['car']['crew_chief'] :
                 crew_chief = entrant['car']['crew_chief']
             else:
                 crew_chief = "na"
