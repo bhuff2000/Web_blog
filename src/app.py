296b460bffbb32967f5ed01d20a102008a125d5f
Bob Houghton <behoughton@yahoo.com>
2020-03-20 14:46:36 -0500

    added socket functionality

--- src/app.py
+++ src/app.py
@@ -29,12 +29,12 @@ def sessions():
     return render_template('draft.html')
 
 
-def messageReceived():
+def messageReceived(methods=['GET', 'POST']):
     print('message was received')
 
 
 @socketio.on('my event')
-def handle_my_custom_event(json):
+def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
     socketio.emit('my response', json, callback=messageReceived)
 
