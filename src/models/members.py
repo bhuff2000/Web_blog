9769db4fdbc0bc2d6e745dda4cb0c02b0e64c6b2
Bob Houghton <behoughton@yahoo.com>
2020-04-07 15:03:45 -0500

    adding new chat room functionality

---
+++ src/models/members.py
@@ -0,0 +1,30 @@
+__author__ = 'behou'
+
+from src.common.database import Database
+import datetime
+
+
+
+class Room_Member(object):
+    def __init__(self, room_id, room_name, username, added_by, is_room_admin):
+        self.room_id = room_id
+        self.room_name = room_name
+        self.username = username
+        self.added_by = added_by
+        self.is_room_admin = is_room_admin
+        self._id = {'room_id': self.room_id, 'username': self.username}
+
+
+    def json(self):
+        return {
+            'room_id': self.room_id,
+            'room_name': self.room_name,
+            'username': self.username,
+            'added_by': self.added_by,
+            'is_room_admin': self.is_room_admin,
+            '_id': self._id
+        }
+
+    def add_room_member(self):
+        member = Database.insert('members', data=self.json())
+        
---
+++ src/models/rooms.py
@@ -0,0 +1,33 @@
+__author__ = 'behou'
+from src.common.database import Database
+import datetime
+import uuid
+
+class Room(object):
+    def __init__(self, room_name, created_by, created_at=datetime.datetime.now(), _id=None):
+        self.room_name = room_name
+        self.created_by = created_by
+        self.created_at = created_at
+        self._id = uuid.uuid4().hex if _id is None else _id
+
+    def json(self):
+        return {
+            'room_name': self.room_name,
+            'created_by': self.created_by,
+            'created_at': self.created_at,
+            '_id': self._id
+        }
+
+    def save_room(self):
+        Database.insert(collection='rooms',
+                        data=self.json())
+    @classmethod
+    def get_room_by_id(cls, _id):
+        room = Database.find_one('rooms', {'room_id': _id})
+        return room
+
+    @classmethod
+    def get_room_by_name(cls, room_name):
+        room = Database.find_one('rooms', {'room_name': room_name})
+        return room
+
