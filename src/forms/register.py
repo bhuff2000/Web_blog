80f189ecc06fdaf58638319715aba40d37666274
Bob Houghton <behoughton@yahoo.com>
2020-04-10 16:09:47 -0500

    adding new chat room functionality

--- src/forms/register.py
+++ src/forms/register.py
@@ -2,8 +2,9 @@ __author__ = 'behou'
 
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError
 from wtforms.validators import DataRequired, Length, email, Regexp, EqualTo
+from src.models.user import User
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64), email()])
@@ -14,3 +15,10 @@ class RegistrationForm(FlaskForm):
     password2 = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Register')
 
+    def validate_email(self, field):
+        if User.get_by_email(field):
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if User.get_by_username(field):
+            raise ValidationError('Username already in use.')
