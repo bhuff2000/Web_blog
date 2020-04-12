c1ad8ed815ab364df2dd29c7db468954e59047b5
Bob Houghton <behoughton@yahoo.com>
2020-04-12 13:16:51 -0500

    adding new chat room functionality

--- src/app.py
+++ src/app.py
@@ -249,8 +249,7 @@ def nascar_pool():
     if form.validate_on_submit():
         new_pool = Room(pool_name, current_user)
         print(new_pool.json())
-        new_pool.save_room()
-        print(new_pool.json())
+        
         return render_template('profile.html', email=session['email'])
     return render_template('create_pool.html', form = form)
 
