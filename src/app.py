b2a2a332b7fe2e06aabb6393e054ae84dff6120d
Bob Houghton <behoughton@yahoo.com>
2020-04-16 18:02:58 -0500

    troubleshooting socketio

--- src/app.py
+++ src/app.py
@@ -63,7 +63,7 @@ def handle_send_message_event(data):
 def handle_join_room_event(data):
     app.logger.info("{} has joined the room {}".format(data['username'], data['room']))
     join_room(data['room'])
-    socketio.emit('join room announcement', data, room=data['room'])
+    socketio.emit('join room announcement', {'username': current_user.username}, room=data['room'])
 
 @socketio.on('leave_room')
 def handle_leave_room_event(data):
