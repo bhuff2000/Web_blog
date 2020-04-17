02371f8b67a06bab497c6bf9746b18163751d3ff
Bob Houghton <behoughton@yahoo.com>
2020-04-17 14:36:50 -0500

    troubleshooting socketio

--- src/forms/create_pool.py
+++ src/forms/create_pool.py
@@ -16,7 +16,7 @@ class CreatePool(FlaskForm):
     #            Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
     #            'Usernames must have only letters, numbers, dots or underscores')])
     members = StringField('Members', validators=[DataRequired(), Length(1, 64)])
-    series = SelectField('Select Series', choices=[('Trucks', 'Trucks'), ('Xfinity','Xfinity'), ('Cup', 'Cup')])
+    series = SelectField('Select Series', choices=[('TRUCK', 'TRUCK'), ('XFINITY','XFINITY'), ('CUP', 'CUP')])
     race = SelectField('Select Race', choices=[('Choose_race', 'Choose Race')])
     submit = SubmitField('Create Pool')
 
