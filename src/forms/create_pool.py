17ae138d8201943ee7ed5cb78b5061053df77f1b
Bob Houghton <behoughton@yahoo.com>
2020-04-19 13:54:43 -0500

    troubleshooting socketio

--- src/forms/create_pool.py
+++ src/forms/create_pool.py
@@ -26,13 +26,13 @@ import wtforms
 
 def get_race_choices():
     year = int(datetime.now().year)
-    print(year)
-    total_races = Sched_Event.find_by_year(year)
-    print(total_races)
-    race_list = []
-    for race in total_races:
-        race_list.append((race['race_id'], race['race_name'] + ' @ ' + race['track']))
-    return race_list
+    #print(year)
+    #total_races = Sched_Event.find_by_year(year)
+    #print(total_races)
+    #race_list = []
+    #for race in total_races:
+    #    race_list.append((race['race_id'], race['race_name'] + ' @ ' + race['track']))
+    return year
 
 class CreatePool(FlaskForm):
 
