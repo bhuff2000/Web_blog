7b367b7a58b7e80234265830b0c2d087a15cd8e8
Bob Houghton <behoughton@yahoo.com>
2020-04-21 12:15:04 -0500

    continue troubleshooting form selectfield

--- src/forms/create_pool.py
+++ src/forms/create_pool.py
@@ -34,7 +34,7 @@ class CreatePool(FlaskForm):
     #            'Usernames must have only letters, numbers, dots or underscores')])
     members = StringField('Members', validators=[DataRequired(), Length(1, 64)])
     series = SelectField('Select Series', choices=[('Choose Series', 'Choose Series'), ('go', 'TRUCKS'), ('xf','XFINITY'), ('sc', 'CUP')])
-    race = SelectField('Select Race',  choices= [])
+    #race = SelectField('Select Race',  choices= [])
     #race = SelectRace.races
     #race = wtforms.FormField(SelectRace)
     submit = SubmitField('Create Pool')
