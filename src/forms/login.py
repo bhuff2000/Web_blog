78705b58845bbb780f84479549b848e9d5956012
Bob Houghton <behoughton@yahoo.com>
2020-04-21 20:38:36 -0500

    continue troubleshooting form selectfield

--- src/forms/login.py
+++ src/forms/login.py
@@ -2,10 +2,10 @@ __author__ = 'behou'
 
 from flask_wtf import FlaskForm, form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, email
+from wtforms.validators import DataRequired, Length, Email
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(1, 64), email()])
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log in')
