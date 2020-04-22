ebbba854d280d2d357be599324379882b488e569
Bob Houghton <behoughton@yahoo.com>
2020-04-21 21:05:36 -0500

    continue troubleshooting form selectfield

--- src/forms/login.py
+++ src/forms/login.py
@@ -5,7 +5,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length, Email
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', [Email(message='not a valid email')],[DataRequired()])
+    email = StringField('Email', Email(),[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log in')
