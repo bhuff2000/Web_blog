548fe47a7d5a01cfee97d33cbe611fbab4cd26a0
Bob Houghton <behoughton@yahoo.com>
2020-04-27 21:45:24 -0500

    troubleshooting login email

--- src/models/entrants.py
+++ src/models/entrants.py
@@ -92,7 +92,7 @@ class Entrants(object):
 
     @classmethod
     def find_drivers_by_race_id(cls, race_id):
-        data = Database.find("entrants", {'race_id': race_id})
+        data = Database.find("entrants", {"race_id": race_id})
         driver_list = []
         for driver in driver_list:
             driver_list.append(driver)
