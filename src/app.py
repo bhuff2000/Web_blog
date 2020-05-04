bae98db8551ef569f28d451bf2e5e657094e8390
Bob Houghton <behoughton@yahoo.com>
2020-05-03 21:11:56 -0500

    troubleshooting login email

--- src/app.py
+++ src/app.py
@@ -71,6 +71,7 @@ def handle_send_message_event(data):
 @socketio.on('start_draft')
 def handle_start_draft(data):
     room_id = data['room_id']
+    print('in socket for pick list creation' + str(room_id))
     members = Draft_Picks.create_and_save_pick_list(room_id)
     print(members)
     socketio.emit('start_draft_annc', {'pick_list': members}, broadcast=True)
