10fe6931e02d5b73a540dc421df24b3126395b9a
Bob Houghton <behoughton@yahoo.com>
2020-05-04 11:18:56 -0500

    troubleshooting login email

--- src/models/draft_picks.py
+++ src/models/draft_picks.py
@@ -44,7 +44,7 @@ class Draft_Picks(object):
     @classmethod
     def pick_order(cls, member_list):
         members = member_list
-        num = members.count(members)
+        num = len(members)
         print(num)
         pick_list =[]
         #seed(1)
