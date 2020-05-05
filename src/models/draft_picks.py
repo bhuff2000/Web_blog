f45ec5ce4f9e9301429b8061ab9eeb3f933d7f5c
Bob Houghton <behoughton@yahoo.com>
2020-05-05 11:11:46 -0500

    troubleshooting login email

--- src/models/draft_picks.py
+++ src/models/draft_picks.py
@@ -85,7 +85,7 @@ class Draft_Picks(object):
             if not (group%2) == 0:
                 for member in ordered_members:
                     pick = Draft_Picks(room_id, member, i, group)
-                    print('pick ' + str(pick) + ' group ' + str(group) + ' odd  ' + str(i))
+                    print('pick ' + str(pick.json()) + ' group ' + str(group) + ' odd  ' + str(i))
                     pick.save_to_mongo()
             else:
                 for member in reversed(ordered_members):
