1785825dde6117b3aeb301e6cdf8e3e54184de6a
Bob Houghton <behoughton@yahoo.com>
2020-05-04 20:23:13 -0500

    troubleshooting login email

--- src/models/draft_picks.py
+++ src/models/draft_picks.py
@@ -44,7 +44,7 @@ class Draft_Picks(object):
     @classmethod
     def pick_order(cls, member_list, room_id):
         #members = member_list
-        print('stuff' + str(members[0]))
+        #print('stuff' + str(members[0]))
         collection = Database.DATABASE['members']
         num = collection.count({"room_id": room_id})
         print(str(num))
