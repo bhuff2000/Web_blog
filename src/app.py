33e3dbb630eb0bb575ba98e08c4d22b2157ea177
Bob Houghton <behoughton@yahoo.com>
2020-05-07 20:56:55 -0500

    troubleshooting login email

--- src/app.py
+++ src/app.py
@@ -81,9 +81,9 @@ def handle_start_draft(data):
 def handle_load_driver_pick(data):
     driver_id = data['driver_id']
     room_id = data['room_id']
-    room_data = Room.get_race_id(room_id)
-    print(str(room_data['_id']))
-    race_id = room_data.race_id
+    race_id = Room.get_race_id(room_id)
+    #print(str(room_data['_id']))
+    #race_id = room_data.race_id
     username = data['username']
     print('in socket for load driver pick' + str(driver_id))
     drv_data = Entrants.find_by_driver_id(race_id, driver_id)
