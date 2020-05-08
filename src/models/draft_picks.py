fdd9fe6403b5d38663f6d9392e0586b12bcb874d
Bob Houghton <behoughton@yahoo.com>
2020-05-08 16:36:15 -0500

    adding load pick to database functionality

--- src/models/draft_picks.py
+++ src/models/draft_picks.py
@@ -36,9 +36,9 @@ class Draft_Picks(object):
         query = {"$and": [{"room_id": room_id}, {"username.username": username}, {"pool_pick_num": pick_num}]}
         #query = {"$and": [{"room_id": room_id}, {"username": username}]}
         print(str(query))
-        #data = Database.find_one("picks", {"$and": [{"room_id": room_id}, {"username": "usernameusername}]})
+        data = Database.find_one("picks", {"$and": [{"room_id": room_id}, {"username.username": username}, {"pool_pick_num": pick_num}]})
         #print(str(data["drv_full"]))
-        #print('find result ' + str(data))
+        print('find result ' + str(data))
         values = {"car_num": car_num, "drv_full": drv_full}
         print(str(values))
         new_vals = {"$set": values}
