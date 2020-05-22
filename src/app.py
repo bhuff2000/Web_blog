6f3799a7e694503b5d5f9e427c617c8e45a725d6
Bob Houghton <behoughton@yahoo.com>
2020-05-21 23:28:50 -0500

    change to pool result

--- src/app.py
+++ src/app.py
@@ -587,8 +587,9 @@ def load_race_results():
             load_list.append(result)
         else:
             record = Results.get_by_race_and_drv_id(test_race_id, test_drv_id)
+            record_json = record.json()
             rec_json =result.json()
-            obj_id_to_replace = record._id
+            obj_id_to_replace = record_json["_id"]
             #obj_id_to_replace = "5ec5f4b93acf8b000bfe6ec3"
             print(str(obj_id_to_replace))
             print(type(obj_id_to_replace))
