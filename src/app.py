70f2406185eac628b59fb126618c5e4f9e02e944
Bob Houghton <behoughton@yahoo.com>
2020-05-28 23:55:47 -0500

    change to pool result

--- src/app.py
+++ src/app.py
@@ -37,7 +37,7 @@ app.config.from_object('src.config')
 app.secret_key = "jose"
 #CORS(app, resources={r"/behoughton.com/*": {"origins": r"/behoughton.com/*"}} )
 #app.config['CORS_HEADERS']='Content-Type'
-CORS(app, origins=[r"behoughton.com/*"])
+CORS(app, resources=r"/behoughton.com/*")
 file_handler = StreamHandler()
 file_handler.setLevel(logging.INFO)
 app.logger.addHandler(file_handler)
