b5b70ded0c6e234d28863c3640d2f98c7f324e49
Bob Houghton <behoughton@yahoo.com>
2020-05-29 09:17:26 -0500

    change to pool result

--- src/app.py
+++ src/app.py
@@ -41,8 +41,9 @@ app.secret_key = "jose"
 file_handler = StreamHandler()
 file_handler.setLevel(logging.INFO)
 app.logger.addHandler(file_handler)
-socketio =SocketIO(app, logger=True, engineio_logger=True, cors_allowed_origins='*', cors_credentials=False)
 CORS(app, resources=r'/behoughton.com/*',origin=r'/behoughton.com/*')
+socketio =SocketIO(app, logger=True, engineio_logger=True, cors_allowed_origins='*', cors_credentials=False)
+
 login_manager = LoginManager()
 login_manager.login_view = '/login'
 login_manager.init_app(app)
