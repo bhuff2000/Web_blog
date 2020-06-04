be3c87a5708d1f62f8fb6e58289143d51e6eb54d
Bob Houghton <behoughton@yahoo.com>
2020-06-03 23:51:34 -0500

    change to pool result

--- src/app.py
+++ src/app.py
@@ -61,14 +61,12 @@ MYLOG = logging.getLogger(__name__)
 @app.after_request
 def add_headers(response):
     response.headers.add('Access-Control-Allow-Origin', 'http://www.behoughton.com/*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization, Accept, Origin, X-LS-CORS-Template, X-LS-Auth_Token, X-LS-Auth-User-Token, X-LS-Sync-Result, X-LS-Sequence, token')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization, Accept, Origin, X-Requested-With, x-api-key, X-LS-CORS-Template, X-LS-Auth_Token, X-LS-Auth-User-Token, X-LS-Sync-Result, X-LS-Sequence, token')
     response.headers.add('Access-Control-Allow-Methods', 'GET, HEAD, POST, OPTIONS, PUT, PATCH, DELETE')
     response.headers.add('Access-Control-Allow-Credentials', 'True')
-    response.headers.add('X-Request-id', '*')
     response.headers.add('X-Forwarded-For', '108.236.43.12')
     response.headers.add('X-Forwarded-Proto', 'http')
     response.headers.add('X-Forwarded-Port', '80')
-    response.headers.add('X-Request-Start', '*')
     MYLOG.debug("request headers " + str(request.headers))
     MYLOG.debug("response headers " + str(response.headers))
 
